01e298153b11b69be04cdad73ec7e206e9a07172
Saïda Boudjerda <harleen@mbp-de-saida.lan>
2019-11-19 16:15:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./component/Header";
+import Modal from "./component/Modal";
+
+import Offers from "./container/Offers";
+import Offer from "./container/Offer";
+import "./App.css";
+
+const App = () => {
+	let token = Cookies.get("token");
+	const [showModal, setShowModal] = useState(false);
+	const [user, setUser] = useState(token);
+
+	const cookie = token => {
+		Cookies.set("token", token);
+	};
+
+	return (
+		<Router>
+			<>
+				{showModal && (
+					<Modal
+						showModal={showModal}
+						setShowModal={setShowModal}
+						cookie={cookie}
+					/>
+				)}
+
+				<Header
+					altImg="Logo Leboncoin"
+					offer="Déposer une annonce"
+					search="Rechercher"
+					myResearch="Mes recherches"
+					favorites="Favoris"
+					messages="Messages"
+					showModal={showModal}
+					setShowModal={setShowModal}
+					setUser={setUser}
+					user={user}
+				/>
+
+				{/* {console.log("token is ", token)}
+				{console.log("user is ", user)} */}
+
+				<Switch>
+					<Route exact path="/">
+						<Offers />
+					</Route>
+
+					<Route path="/Offer/:id">
+						<Offer />
+					</Route>
+				</Switch>
+			</>
+		</Router>
+	);
+};
 
 export default App;
